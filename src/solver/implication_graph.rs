7cafbe585150d82a8aeda80f1ffe4eee42c6775a
Trent Clarke <trent.clarke@gmail.com>
2015-02-14 22:41:22 +1100

    Added a slightly more sophisticated clause-learning algorithm

--- src/solver/implication_graph.rs
+++ src/solver/implication_graph.rs
@@ -30,12 +30,12 @@ use collections::VecSet;
 //
 // ----------------------------------------------------------------------------
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Show)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Debug)]
 struct Assignment(Var, SolutionValue);
 
 /**
  * An abbreviated assignment (ha!), useful for consructing assignment literals
- * when writing tests. 
+ * when writing tests.
  */
 type Asmt = (Var, SolutionValue);
 
@@ -57,7 +57,7 @@ type Assignments = Vec<Assignment>;
 //
 // ----------------------------------------------------------------------------
 
-#[derive(Show)]
+#[derive(Debug)]
 struct Implication {
 	level: usize,
 	roots: Vec<Assignment>
@@ -69,8 +69,7 @@ struct Implication {
 
 type GraphImpl = HashMap<Assignment, Implication>;
 
-
-#[derive(Show)]
+#[derive(Debug)]
 pub struct ImplicationGraph {
     map: GraphImpl
 }
@@ -90,24 +89,30 @@ impl ImplicationGraph {
     }
 
     pub fn insert(&mut self,
-    	          level: usize, 
-    	          var: Var, 
-    	          val: SolutionValue, 
-    			  roots: &[Asmt]) 
+    	          level: usize,
+    	          var: Var,
+    	          val: SolutionValue,
+    			  roots: &[Asmt])
     {
-    	self.map.insert(
-    		Assignment(var, val), 
-    		Implication { 
-    			level: level, 
-    			roots: roots.iter()
+        let asmt = Assignment(var, val);
+        let i = Implication {
+                level: level,
+                roots: roots.iter()
                             .map(mk_assignment)
                             .collect()
-    		});
+                };
+
+        match self.map.insert(asmt, i) {
+            None => {},
+            Some(_) => {
+                panic!("Assignment for {:?} already exists!", asmt);
+            }
+        }
     }
 
     pub fn remove(&mut self, var: Var, val: SolutionValue) {
     	self.map.remove(&Assignment(var, val));
-    } 
+    }
 
     pub fn learn_conflict_clause(&self, conflict: Var, decision: Asmt) -> Clause {
         dump_graph("learn_conflict.dot", self);
@@ -118,7 +123,7 @@ impl ImplicationGraph {
      * Does this graph have an implication for the given variable.
      */
     pub fn has(&self, v: Var) -> bool {
-        self.map.get(&Assignment(v, False)).is_some() || 
+        self.map.get(&Assignment(v, False)).is_some() ||
         self.map.get(&Assignment(v, True)).is_some()
     }
 
@@ -141,7 +146,7 @@ fn mk_assignment(asmt: &Asmt) -> Assignment {
 
 #[config(test)]
 fn test_graph() -> ImplicationGraph {
-    // graph from 
+    // graph from
     // http://www.cs.princeton.edu/courses/archive/fall13/cos402/readings/SAT_learning_clauses.pdf
     // Section 4: Learning Schemes
     ImplicationGraph::from(&[
@@ -155,7 +160,7 @@ fn test_graph() -> ImplicationGraph {
         (4, (5, True),  &[(8, False), (4, True)]),
         (4, (6, True),  &[(9, False), (4, True)]),
         (4, (5, False), &[(6, True)])
-    ])   
+    ])
 }
 
 #[test]
@@ -180,38 +185,38 @@ fn learn_conflict_clause(g: &GraphImpl, v: Var, decision: Assignment) -> Clause 
     let level = g.get(&decision).unwrap().level;
 
     let negative_assignment = Assignment(v, False);
-    let negative_path = find_path(g, negative_assignment, decision).unwrap();
+    let negative_paths = find_paths(g, negative_assignment, decision);
 
     let positive_assignment = Assignment(v, True);
-    let positive_path = find_path(g, positive_assignment, decision).unwrap();
-    
-    let uip = find_first_uip(g, level, &negative_path, &positive_path);
+    let positive_paths = find_paths(g, positive_assignment, decision);
 
-    // ok, so now we have a point we can cut at. We now need to find all of the 
-    // edges in the graph that cross the cut. For example, given the graph 
+    let uip = find_first_uip(g, level, &negative_paths[0], &positive_paths[0]);
+
+    // ok, so now we have a point we can cut at. We now need to find all of the
+    // edges in the graph that cross the cut. For example, given the graph
     // (where the cut is marked with an asterisk)
     //
-    //            8  
+    //            8
     //              \
     //       2        5+
-    //     /   \    / 
+    //     /   \    /
     //   1       4 *      5-
-    //     \   /    \   / 
-    //       3        6 
+    //     \   /    \   /
+    //       3        6
     //     /        /
     //   7        9
     //
-    // ...the nodes 5+, 5- & 6 are on one side of the cut, and everything else is 
-    // on the other. The nodes on with edges that cross the cut (4, 8 & 9) are 
+    // ...the nodes 5+, 5- & 6 are on one side of the cut, and everything else is
+    // on the other. The nodes on with edges that cross the cut (4, 8 & 9) are
     // the ones we're interested in.
     //
-    // We know the path from both 5+ and 5- to 4, so we can assume that any 
+    // We know the path from both 5+ and 5- to 4, so we can assume that any
     // nodes are on those paths are on the conflict side, so we can simply walk
-    // the graph from 5+/- backwards. If we hit a node that is not in the 
+    // the graph from 5+/- backwards. If we hit a node that is not in the
     // conflict side, we add that to the learned conflict clause.
 
     // NB  - make some sort of generic BFS.
-    let conflict_side_vars = mk_conflict_set(&negative_path, &positive_path, uip);
+    let conflict_side_vars = mk_conflict_set(&negative_paths, &positive_paths, uip);
     let mut conflict_clause : VecSet<Term> = VecSet::new();
     for v in conflict_side_vars.iter() {
         let i = g.get(v).unwrap();
@@ -229,6 +234,49 @@ fn learn_conflict_clause(g: &GraphImpl, v: Var, decision: Assignment) -> Clause 
 
     FromIterator::from_iter(
         conflict_clause.iter().map(|&x| x))
+}
+
+#[test]
+fn learned_clause_considers_all_nodes_in_conflict_side() {
+    //       13
+    //         \
+    //    12   14      15
+    //     |   /         \
+    //    11  *3 - *8 - *~7
+    //     | /
+    // 1 - 2 - 5 - 6 - *7
+    //       \         /
+    //        +- *4 -+
+    //           /
+    //     10 - 9
+
+    // The conflict side contains any path from +/~7 back to the first uip at
+    // 2, and so the resulting learned clause should include 2, 9, 15 and
+    // nothing else.
+    let g = ImplicationGraph::from(&[
+        (1, ( 1, False), &[]),
+        (1, (10, False), &[]),
+        (1, (13, False), &[]),
+        (1, (15, False), &[]),
+        (1, ( 2, False), &[(1, False)]),
+        (1, (11, False), &[(2, False)]),
+        (1, (12, False), &[(12, False)]),
+        (1, ( 3, False), &[(2, False)]),
+        (1, ( 8, False), &[(3, False)]),
+        (1, ( 7, False), &[(8, False), (15, False)]),
+        (1, (14, False), &[(3,False), (13,False)]),
+        (1, ( 9, False), &[(10, False)]),
+        (1, ( 4, False), &[(2, False), (9, False)]),
+        (1, ( 5, False), &[(2, False)]),
+        (1, ( 6, False), &[(5, False)]),
+        (1, ( 7, True),  &[(6, False), (4, False)]),
+    ]);
+
+    let clause = learn_conflict_clause(&g.map, 7, Assignment::new(1, False));
+    assert!(clause.len() == 3);
+    assert!(clause.iter().any(|t| *t == Lit(2)), "Clause must contain 2");
+    assert!(clause.iter().any(|t| *t == Lit(9)), "Clause must contain 9");
+    assert!(clause.iter().any(|t| *t == Lit(15)), "Clause must contain 15");
 }
 
 #[test]
@@ -255,34 +303,38 @@ fn learn_clause_finds_expected_clause_from_wikipedia_example() {
 
 #[test]
 fn learn_clause_finds_expected_clause_from_princeton_example() {
-
     let g = test_graph();
-
     let clause = learn_conflict_clause(&g.map, 5, Assignment::new(1, False));
     assert_eq!(3, clause.len());
     assert!(clause.iter().any(|t| *t == Not(4)), "Clause must contain ~4");
     assert!(clause.iter().any(|t| *t == Lit(8)), "Clause must contain 8");
-    assert!(clause.iter().any(|t| *t == Lit(9)), "Clause must contain 9");    
+    assert!(clause.iter().any(|t| *t == Lit(9)), "Clause must contain 9");
 }
 
-fn mk_conflict_set(a: &Assignments, b: &Assignments, uip: Assignment) -> VecSet<Assignment> {
+/**
+ * Collects the set of nodes in the conflict side of the cut.
+ */
+fn mk_conflict_set(a: &Vec<Assignments>,
+                   b: &Vec<Assignments>,
+                   uip: Assignment) -> VecSet<Assignment> {
     let mut result = VecSet::new();
-    result.extend(a.iter().take_while(|&x| *x != uip).map(|&x| x));
-    result.extend(b.iter().take_while(|&x| *x != uip).map(|&x| x));
+    for v in a.iter().chain(b.iter()) {
+        result.extend(v.iter().take_while(|&x| *x != uip).map(|&x| x));
+    }
     result
 }
 
 #[test]
 fn mk_conflict_set_doesnt_include_uip() {
-    let path_a = mk_assignments(&[(5,True), (4,True), (2,True), (1, False)]);
-    let path_b = mk_assignments(&[(5,False), (6, True), (4,True), (3,True), (1, False)]);
+    let path_a = vec!(mk_assignments(&[(5,True), (4,True), (2,True), (1, False)]));
+    let path_b = vec!(mk_assignments(&[(5,False), (6, True), (4,True), (3,True), (1, False)]));
     let uip = Assignment(4, True);
     let conflict_set = mk_conflict_set(&path_a, &path_b, uip);
     let expected : VecSet<Assignment> = FromIterator::from_iter(
         mk_assignments(&[(5, True), (5, False), (6, True)])
             .iter()
             .map(|&x| x)
-    ); 
+    );
 
     assert!(conflict_set == expected);
 }
@@ -293,7 +345,7 @@ fn mk_conflict_set_doesnt_include_uip() {
 
 fn find_first_uip(g: &GraphImpl,
                   level: usize,
-                  negative_path: &Assignments, 
+                  negative_path: &Assignments,
                   positive_path: &Assignments) -> Assignment {
     for asmt in negative_path.iter() {
         let l = g.get(asmt).unwrap().level;
@@ -311,9 +363,9 @@ fn find_first_uip(g: &GraphImpl,
 fn find_first_uip_picks_correct_point() {
     let g = test_graph();
     let decision = Assignment(1, False);
-    let negative_path = find_path(&g.map, Assignment(5, False), decision).unwrap();
-    let positive_path = find_path(&g.map, Assignment(5, True), decision).unwrap();
-    let Assignment(var, val) = find_first_uip(&g.map, 4, &negative_path, &positive_path);
+    let negative_paths = find_paths(&g.map, Assignment(5, False), decision);
+    let positive_paths = find_paths(&g.map, Assignment(5, True), decision);
+    let Assignment(var, val) = find_first_uip(&g.map, 4, &negative_paths[0], &positive_paths[0]);
     assert_eq!(4, var);
     assert_eq!(True, val);
 }
@@ -323,21 +375,19 @@ fn find_first_uip_picks_correct_point() {
 // ----------------------------------------------------------------------------
 
 /**
- * Finds a path through an implication graph, from a given source to a given 
- * destination. Should find the shortest available path.
+ * Finds all paths through an implication graph, from a given source to a given
+ * destination. Returns the paths shortest to longest. Returns an empty vector
+ * if no paths are found.
+ *
+ * The graph must be acyclic, or we'll be here all day.
  */
-fn find_path(g: &GraphImpl, src: Assignment, dst: Assignment) -> Option<Assignments> {
-    let mut result : Assignments = Vec::new();
-    let mut visited : VecSet<Assignment> = VecSet::new();
+fn find_paths(g: &GraphImpl, src: Assignment, dst: Assignment) -> Vec<Assignments> {
+    let mut result : Vec<Assignments> = Vec::new();
     let mut queue : RingBuf<(Assignment, Assignments)> = RingBuf::new();
     queue.push_back((src, vec!(src)));
 
     while !queue.is_empty() {
         let (parent, path_to_parent) = queue.pop_front().unwrap();
-
-        if !visited.insert(parent) { 
-            continue; 
-        }
 
         match g.get(&parent) {
             None => {},
@@ -346,15 +396,17 @@ fn find_path(g: &GraphImpl, src: Assignment, dst: Assignment) -> Option<Assignme
                     let mut path = path_to_parent.clone();
                     path.push(child);
                     if dst == child {
-                        return Some(path)
+                        result.push(path)
                     }
-                    queue.push_back((child, path));
+                    else {
+                        queue.push_back((child, path));
+                    }
                 }
             }
         }
     }
 
-    return None
+    return result
 }
 
 #[config(test)]
@@ -365,16 +417,27 @@ fn mk_assignments(asmts: &[Asmt]) -> Assignments {
 }
 
 #[test]
-fn find_path_finds_expected_path() {
-    let g = test_graph();
-    let path = find_path(&g.map, Assignment(5, True), Assignment(1, False)).unwrap();
-    let path_a = mk_assignments(&[(5,True), (4,True), (2,True), (1, False)]);
-    let path_b = mk_assignments(&[(5,True), (4,True), (3,True), (1, False)]);
-    assert!((path_a == path) || (path_b == path));
+fn find_path_finds_all_paths() {
+    //  /-- 1 --- 2 --- 5 --\
+    // 0             \       6
+    //  \-- 3 --------- 4 --/
+    //
+    let mut g = ImplicationGraph::from(&[
+        (1, (0, False), &[]),
+        (1, (1, False), &[(0, False)]),
+        (1, (2, False), &[(1, False)]),
+        (1, (5, False), &[(2, False)]),
+        (1, (3, False), &[(0, False)]),
+        (1, (4, False), &[(3, False), (2, False)]),
+        (1, (6, False), &[(4, False), (5,False)])
+    ]);
+
+    let paths = find_paths(&g.map, Assignment(6, False), Assignment(0, False));
+    assert_eq!(3, paths.len());
 }
 
 #[test]
-fn find_path_finds_shortest_path() {
+fn find_paths_finds_shortest_path_first() {
     //  /-- 1 --- 2 --- 5 --\
     // 0                     6
     //  \-- 3 --------- 4 --/
@@ -388,12 +451,14 @@ fn find_path_finds_shortest_path() {
         (1, (4, False), &[(3, False)]),
         (1, (6, False), &[(4, False), (5,False)])
     ]);
-    
+
     //dump_graph("shortest.dot", &g);
 
-    let path = find_path(&g.map, Assignment(6, False), Assignment(0, False)).unwrap();
+    let paths = find_paths(&g.map, Assignment(6, False), Assignment(0, False));
     let expected = mk_assignments(&[(6, False), (4, False), (3, False), (0, False)]);
-    assert!(path == expected);
+    assert_eq!(2, paths.len());
+    assert!(paths[0] <= paths[1]);
+    assert!(paths[0] == expected);
 }
 
 
@@ -436,20 +501,20 @@ impl<'a> dot::GraphWalk<'a, Node, Edge> for ImplicationGraph {
         let mut edges = Vec::new();
         for (asmt, implication) in self.map.iter() {
             for root in implication.roots.iter() {
-                edges.push((root.clone(), asmt.clone()))            
+                edges.push((root.clone(), asmt.clone()))
             }
         }
         Cow::Owned(edges)
     }
 
-    fn source(&self, e: &Edge) -> Node { 
-        let &(ref src, _) = e; 
-        src.clone() 
+    fn source(&self, e: &Edge) -> Node {
+        let &(ref src, _) = e;
+        src.clone()
     }
 
-    fn target(&self, e: &Edge) -> Node { 
-        let &(_, ref dst) = e; 
-        dst.clone() 
+    fn target(&self, e: &Edge) -> Node {
+        let &(_, ref dst) = e;
+        dst.clone()
     }
 }
 
